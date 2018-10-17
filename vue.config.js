bc6cb47d046f8abbb78a80efc1da737d2c4de92d
Renato Hormazabal <renato.hormazabal@schibsted.com>
2018-10-17 10:24:28 -0300

    build(init-page): changed asset dir

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  baseUrl: '/messaging-center/app'
+  baseUrl: '/messaging-center/app',
+  outputDir: undefined,
+  assetsDir: '/messaging-center/app',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
