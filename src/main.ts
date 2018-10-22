aa5d9892204b2e14511e5d6abf8ed014bf55187c
Renato Hormazabal <renato.hormazabal@schibsted.com>
2018-10-22 18:18:38 -0300

    feat(load-xiti-tag): loaded tealium by main script

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from '@/store';
 import router from '@/router';
+import tealiumLoad from '@/utils/tealiumLoad';
 
 Vue.config.productionTip = false;
+
+const checkEnv = () => /yapo\.cl/gm.exec(window.location.origin);
+tealiumLoad(!checkEnv ? 'prod' : 'dev');
 
 new Vue({
   router,
--- src/utils/tealiumLoad.ts
+++ src/utils/tealiumLoad.ts
@@ -1,6 +1,5 @@
-/*
-export function tealiumLoad(){
-  const url: string = '//tags.tiqcdn.com/utag/[ACCOUNT]/[PROFILE]/[ENV]/utag.js';
+export default function tealiumLoad(env: string) {
+  const url: string = '//tags.tiqcdn.com/utag/schibsted/yapo/' + env + '/utag.js';
   const target: Document = document;
   const type: string = 'script';
   const element: any = target.createElement(type);
@@ -8,8 +7,7 @@ export function tealiumLoad(){
   element.type = 'text/java' + type;
   element.async = true;
   const element2: Element = target.getElementsByTagName(type)[0];
-  if (element2 !== null) {
-    element2.parentNode.insertBefore( element, a);
+  if (element2 !== null && element2.parentNode !== null) {
+    element2.parentNode.insertBefore( element, element2);
   }
-};
-*/
+}
