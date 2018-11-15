01af4b6f6ac38129ad4ad38ecbafa2f064956da8
Renato Hormazabal <renato.hormazabal@schibsted.com>
2018-11-15 09:08:51 -0300

    style(ui-messaging): added yapo ui on widget
    https://jira.schibsted.io/browse/SD-368

---
+++ mock/db.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+  return {
+    block: require('./api/block.json'),
+    messages: require('./api/messages.json'),
+    conversations: require('./api/conversations.json'),
+    conversation: require('./api/conversation.json'),
+    images: require('./api/images.json'),
+    configurations: require('./api/configurations.json')
+  }
+}
---
+++ mock/middleware.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-param-reassign */
+module.exports = (req, res, next) => {
+  if (req.path === '/messaging/api/v1/api/hal/xuser/attachments/access') {
+    const images = require('./api/images.json');
+    var img = new Buffer(images[req.query.objectPath], 'base64');
+    res.header("Content-Type", "image/png");
+    res.header("Content-Length", img.length);
+    var request = require('request').defaults({ encoding: null });
+    request.get(images[req.query.objectPath], function (err, resRe, body) {
+      res.send(body);
+    });
+  } else {
+    next();
+  }
+};
