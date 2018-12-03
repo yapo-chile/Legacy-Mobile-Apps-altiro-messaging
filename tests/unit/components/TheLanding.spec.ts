7edd0c7068acbd545a582c433282b701f5c5b92d
Renato Hormazabal <renato.hormazabal@schibsted.com>
2018-12-03 14:58:17 -0300

    test(menu-header): fixed theLanding test

--- tests/unit/components/TheLanding.spec.ts
+++ tests/unit/components/TheLanding.spec.ts
@@ -4,6 +4,9 @@ import TheLanding from '@/components/TheLanding.vue';
 import { MessagingState } from '@/store/modules/messaging/types';
 import { mutations } from '@/store/modules/messaging/mutations';
 import { getters } from '@/store/modules/messaging/getters';
+import { AuthState } from '@/store/modules/auth/types';
+import { getters as aGetters } from '@/store/modules/auth/getters';
+import { mutations as aMutations } from '@/store/modules/auth/mutations';
 import { config } from '@/assets/messagingConfig';
 
 const localVue = createLocalVue();
@@ -11,6 +14,7 @@ localVue.use(Vuex);
 
 describe('TheLanding test', () => {
   let state: MessagingState;
+  let authState: AuthState;
   let actions: any;
   let store: any;
   let wrapper: any;
@@ -20,6 +24,16 @@ describe('TheLanding test', () => {
     state = {
       config,
     };
+
+    authState = {
+      userName: '',
+      shortName: '',
+      userEmail: '',
+      accSession: '',
+      avatar: '',
+      isLoggedIn: false,
+    };
+
     actions = {};
 
     store = new Vuex.Store({
@@ -31,9 +45,22 @@ describe('TheLanding test', () => {
           mutations,
           namespaced,
         },
+        auth: {
+          state: authState,
+          actions,
+          getters: aGetters,
+          mutations: aMutations,
+          namespaced,
+        },
       },
     });
-    wrapper = shallowMount(TheLanding, { store, localVue });
+    wrapper = shallowMount(TheLanding, {
+      store,
+      localVue,
+      beforeCreate() {
+        (this as any).$t = () => ('mock-i18n');
+      },
+    });
   });
   it('should have a messaging-widget element', () => {
     expect(wrapper.contains('.landing-messaging')).toBeTruthy();
