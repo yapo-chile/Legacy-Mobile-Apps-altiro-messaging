d43702886605442abfb0e32cda94bd54e69e4a57
Renato Hormazabal <renato.hormazabal@schibsted.com>
2018-12-05 09:10:51 -0300

    fix(menu-header): changed name of i18n reference

--- src/components/TheLanding.vue
+++ src/components/TheLanding.vue
@@ -69,142 +69,145 @@
   </div>
 </template>
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import { Action, State } from 'vuex-class';
-  import { MessagingState } from '@/store/modules/messaging/types';
-  import { config } from '@/assets/messagingConfig';
-  import { messaging } from '@/store/modules/messaging';
-  import { MessagingConfig } from '@/domain/entities/MessagingEntity';
-  import MessagingWidget from './MessagingWidget.vue';
-  import { AuthState } from '@/store/modules/auth/types';
-  import tags from '@/utils/Tealium';
-  import utils from '@/utils/utils';
+import { Component, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
+import { MessagingState } from '@/store/modules/messaging/types';
+import { config } from '@/assets/messagingConfig';
+import { messaging } from '@/store/modules/messaging';
+import { MessagingConfig } from '@/domain/entities/MessagingEntity';
+import MessagingWidget from './MessagingWidget.vue';
+import { AuthState } from '@/store/modules/auth/types';
+import tags from '@/utils/Tealium';
+import utils from '@/utils/utils';
 
-  const namespace: string = 'the-landing';
+const namespace: string = 'the-landing';
 
-  @Component({
-    components: {
-      MessagingWidget,
-    },
-  })
-  export default class TheLanding extends Vue {
-    @State('messaging')
-    public messaging!: MessagingState;
+@Component({
+  components: {
+    MessagingWidget,
+  },
+})
+export default class TheLanding extends Vue {
+  @State('messaging')
+  public messaging!: MessagingState;
 
-    @State('auth')
-    public auth!: AuthState;
+  @State('auth')
+  public auth!: AuthState;
 
-    @Action('setConfig', { namespace: 'messaging' })
-    private setConfig: any;
+  @Action('setConfig', { namespace: 'messaging' })
+  private setConfig: any;
 
-    @Action('logoutUser', { namespace: 'auth' })
-    private logoutUser: any;
+  @Action('logoutUser', { namespace: 'auth' })
+  private logoutUser: any;
 
-    private homeUrl: string = '';
-    private url: string = '';
-    private secureUrl: string = '';
-    private publishUrl: string = '';
-    private loginUrl: string = '';
+  private homeUrl: string = '';
+  private url: string = '';
+  private secureUrl: string = '';
+  private publishUrl: string = '';
+  private loginUrl: string = '';
 
-    // content variables
-    private messagingUrl: string = '';
-    private myAdsUrl: string = '';
-    private myFavoritesUrl: string = '';
-    private myAccountUrl: string = '';
-    private yaPesosUrl: string = '';
-    private buyUrl: string = '';
-    private logoutUrl: string = '';
-    private helpUrl: string = '';
+  // content variables
+  private messagingUrl: string = '';
+  private myAdsUrl: string = '';
+  private myFavoritesUrl: string = '';
+  private myAccountUrl: string = '';
+  private yaPesosUrl: string = '';
+  private buyUrl: string = '';
+  private logoutUrl: string = '';
+  private helpUrl: string = '';
 
-    private beforeMount() {
-      this.url = utils.getUrl();
-      this.secureUrl = utils.getSecureUrl();
-      this.homeUrl = this.url;
-      this.publishUrl = this.secureUrl + '/ai';
-      this.loginUrl = this.secureUrl + '/login';
-      this.messagingUrl = this.url + '/messaging/app';
-      this.myAdsUrl = this.secureUrl + '/dashboard';
-      this.myFavoritesUrl = this.url + '/favoritos';
-      this.myAccountUrl = this.secureUrl + '/cuenta/view';
-      this.yaPesosUrl = this.secureUrl + '/yapesos';
-      this.buyUrl = this.secureUrl + '/pagos/form/';
-      this.logoutUrl = this.secureUrl + '/logout?exit=1';
-      this.helpUrl = 'https://ayuda.yapo.cl/hc/es';
-    }
-    private async created() {
-      await this.setConfig({ ... config,
-        format: {
-          messageDate: this.formatMessageDate,
+  private beforeMount() {
+    this.url = utils.getUrl();
+    this.secureUrl = utils.getSecureUrl();
+    this.homeUrl = this.url;
+    this.publishUrl = this.secureUrl + '/ai';
+    this.loginUrl = this.secureUrl + '/login';
+    this.messagingUrl = this.url + '/messaging/app';
+    this.myAdsUrl = this.secureUrl + '/dashboard';
+    this.myFavoritesUrl = this.url + '/favoritos';
+    this.myAccountUrl = this.secureUrl + '/cuenta/view';
+    this.yaPesosUrl = this.secureUrl + '/yapesos';
+    this.buyUrl = this.secureUrl + '/pagos/form/';
+    this.logoutUrl = this.secureUrl + '/logout?exit=1';
+    this.helpUrl = 'https://ayuda.yapo.cl/hc/es';
+  }
+  private async created() {
+    await this.setConfig({ ... config,
+      format: {
+        messageDate: this.formatMessageDate,
+      },
+      headers: {
+        Authorization: this.auth.accSession,
+      },
+      translations: {
+        status: {
+          message: {
+            error: Vue.i18n.translate('MESSAGING_CENTER.ERRORS.DELIVERY_FAILURE', {}),
+          },
         },
-        headers: {
-          Authorization: this.auth.accSession,
+        action: {
+          block: Vue.i18n.translate('MESSAGING_CENTER.ACTIONS.BLOCK', {}),
+          delete: Vue.i18n.translate('MESSAGING_CENTER.ACTIONS.DELETE', {}),
         },
-        translations: {
-          status: {
-            message: {
-              error: Vue.i18n.translate('MESSAGING_CENTER.ERRORS.DELIVERY_FAILURE', {}),
+        info: {
+          writeYourMessage: Vue.i18n.translate('MESSAGING_CENTER.INFO.WRITE_YOUR_MESSAGE', {}),
+          attachment: {
+            sentYou: {
+              single: Vue.i18n.translate('MESSAGING_CENTER.INFO.ATTACHMENT.SENT_YOU.SINGLE', {}),
+              plural: Vue.i18n.translate('MESSAGING_CENTER.INFO.ATTACHMENT.SENT_YOU.PLURAL',
+                {
+                  numberAttachments: '%{numberAttachments}',
+                }),
             },
-          },
-          action: {
-            block: Vue.i18n.translate('MESSAGING_CENTER.ACTIONS.BLOCK', {}),
-            delete: Vue.i18n.translate('MESSAGING_CENTER.ACTIONS.DELETE', {}),
-          },
-          info: {
-            writeYourMessage: Vue.i18n.translate('MESSAGING_CENTER.INFO.WRITE_YOUR_MESSAGE', {}),
-            attachment: {
-              sentYou: {
-                single: Vue.i18n.translate('MESSAGING_CENTER.INFO.SENT_YOU.SINGLE', {}),
-                plural: Vue.i18n.translate('MESSAGING_CENTER.INFO.ATTACHMENT.SENT_YOU.PLURAL', {}),
-              },
-              file: Vue.i18n.translate('MESSAGING_CENTER.INFO.ATTACHMENT.FILE', {}),
-            },
-          },
-          error: {
-            adNotAvailable: Vue.i18n.translate('MESSAGING_CENTER.ERRORS.AD_NOT_AVAILABLE', {}),
-            uploadFailed: Vue.i18n.translate('MESSAGING_CENTER.UPLOAD_FAILED', {}),
-          },
-          interaction: {
-            confirmDelete:  Vue.i18n.translate('MESSAGING_CENTER.INTERACTION.CONFIRM_DELETE', {}),
+            file: Vue.i18n.translate('MESSAGING_CENTER.INFO.ATTACHMENT.FILE', {}),
           },
         },
-        track: (event: string, payload: any): void => {
-          utag.link({ event_name: 'messaging_center_app', data: {event, payload}});
+        error: {
+          adNotAvailable: Vue.i18n.translate('MESSAGING_CENTER.ERRORS.AD_NOT_AVALAIBLE', {}),
+          uploadFailed: Vue.i18n.translate('MESSAGING_CENTER.ERRORS.UPLOAD_FAILED', {}),
         },
-      });
-    }
+        interaction: {
+          confirmDelete:  Vue.i18n.translate('MESSAGING_CENTER.INTERACTION.CONFIRM_DELETE', {}),
+        },
+      },
+      track: (event: string, payload: any): void => {
+        utag.link({ event_name: 'messaging_center_app', data: {event, payload}});
+      },
+    });
+  }
 
-    private async redirect(url: string, secure: boolean) {
-      if (!secure || this.auth.isLoggedIn) {
-        if (url === this.logoutUrl) {
-          await this.logoutUser();
-        }
-        window.location.assign(url);
-      } else {
-        window.location.assign(this.loginUrl);
+  private async redirect(url: string, secure: boolean) {
+    if (!secure || this.auth.isLoggedIn) {
+      if (url === this.logoutUrl) {
+        await this.logoutUser();
       }
-    }
-
-    private formatMessageDate(data: any): string {
-      let displayedDate;
-
-      if (data.isToday) {
-        displayedDate = `<strong>${data.translator('info.today')}</strong>, ${data.format('hh:mm', data.date)}`;
-      } else {
-        if (data.isYesterday) {
-          displayedDate = `<strong>${data.translator('info.yesterday')}</strong>, ${data.format('hh:mm', data.date)}`;
-        } else {
-          displayedDate = `<strong>${data.format('dd-MM-yyyy', data.date)}</strong>, ${data.format('hh:mm', data.date)}`;
-        }
-      }
-      return displayedDate;
-    }
-
-    private clickEvent(type: string) {
-      tags.link({
-        event_name: 'messaging_center_click',
-      });
+      window.location.assign(url);
+    } else {
+      window.location.assign(this.loginUrl);
     }
   }
+
+  private formatMessageDate(data: any): string {
+    let displayedDate;
+
+    if (data.isToday) {
+      displayedDate = `<strong>${data.translator('info.today')}</strong>, ${data.format('hh:mm', data.date)}`;
+    } else {
+      if (data.isYesterday) {
+        displayedDate = `<strong>${data.translator('info.yesterday')}</strong>, ${data.format('hh:mm', data.date)}`;
+      } else {
+        displayedDate = `<strong>${data.format('dd-MM-yyyy', data.date)}</strong>, ${data.format('hh:mm', data.date)}`;
+      }
+    }
+    return displayedDate;
+  }
+
+  private clickEvent(type: string) {
+    tags.link({
+      event_name: 'messaging_center_click',
+    });
+  }
+}
 </script>
 <style lang="scss" scoped>
   @import "../styles/TheLanding";
